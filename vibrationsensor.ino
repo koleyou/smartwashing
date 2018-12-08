e5f55da3d880c9a963d698e26296e669fe24889e
sellis5566 <ell12019@byui.edu>
2018-12-07 18:03:29 -0700

    Add files via upload

---
+++ vibrationsensor.ino
@@ -0,0 +1,66 @@
+  
+const int Sensor = A0;
+int sensorReading = 0;      // Stores the value read from the sensor pin          
+int washer = 0;             //Is the washer running? 
+int spin = 0;
+int isdone = 0;
+void setup() 
+{
+  Serial.begin(115200);       // use the serial port
+  
+}
+
+void loop() 
+{
+  sensorReading = analogRead(Sensor); 
+  if(sensorReading > 2)
+  {
+    washer = 1;
+    Serial.println("cycle start");
+  }
+    while(washer == 1)
+    {
+     float sum = 0;     //the sum of all the data over a cirtain amaount of time. 
+     float oneup = 0;   //number to track what to devide the average by. 
+    
+     for(int i = 0; i < 600; i++)
+     { 
+        sensorReading = analogRead(Sensor); 
+        if (sensorReading >= 1)
+        { 
+         isdone = 0;    //number to track if its done or not.
+         sum = sum + sensorReading;
+         oneup = oneup + 1;
+         if (sensorReading >= 400)
+         {
+          spin = 1;
+          }
+        }
+        delay(100);
+      }
+      
+      if(sum != 0)
+      {
+        Serial.println(sum / oneup);
+      }
+      else
+      {
+        if(spin == 1)
+        {
+        isdone = isdone + 1;
+        Serial.println(isdone);
+        delay(50);
+        Serial.println("done yet?");
+        if(isdone == 4)
+        {
+          delay(50);
+          Serial.println("washer done");
+          washer = 0;
+        }
+      }  
+      }
+    delay(100);  // check every 15 seconds
+    }
+      
+  delay(100);
+}  
