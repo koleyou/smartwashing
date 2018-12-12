93d6de23eed8d85d00b34db316622e231bbc0277
sellis5566 <ell12019@byui.edu>
2018-12-11 20:42:46 -0700

    Update vibrationsensor.ino

--- vibrationsensor.ino
+++ vibrationsensor.ino
@@ -2,8 +2,8 @@
 const int Sensor = A0;
 int sensorReading = 0;      // Stores the value read from the sensor pin          
 int washer = 0;             //Is the washer running? 
-int spin = 0;               //has the washer doen the spin cylce
-int isdone = 0;             //is the washer done
+int spin = 0;
+int isdone = 0;
 void setup() 
 {
   Serial.begin(115200);       // use the serial port
@@ -13,25 +13,25 @@ void setup()
 void loop() 
 {
   sensorReading = analogRead(Sensor); 
-  if(sensorReading > 2)              //sense when whasher has started.
+  if(sensorReading >= 3)
   {
-    washer = 1;                      //set washer to one to start the rest of code.
-    Serial.println("cycle start");
+    washer = 1;
+    Serial.println("c");
   }
     while(washer == 1)
     {
      float sum = 0;     //the sum of all the data over a cirtain amaount of time. 
-     float oneup = 0;   //number to track what to divide the average by. 
+     float oneup = 0;   //number to track what to devide the average by. 
     
-     for(int i = 0; i < 600; i++)
+     for(int i = 0; i < 400; i++)
      { 
         sensorReading = analogRead(Sensor); 
-        if (sensorReading >= 1)             // don't count any 0's
+        if (sensorReading >= 1)
         { 
-         isdone = 0;                        //number to track if its done or not.
-         sum = sum + sensorReading;         // add up all the sensor readings
-         oneup = oneup + 1;                 //number to divide the sum by to get average.
-         if (sensorReading >= 400)          //logic to tell if spin cylce has occured. 
+         isdone = 0;    //number to track if its done or not.
+         sum = sum + sensorReading;
+         oneup = oneup + 1;
+         if (sensorReading >= 400)
          {
           spin = 1;
           }
@@ -39,9 +39,9 @@ void loop()
         delay(100);
       }
       
-      if(sum != 0)                         
+      if(sum != 0)
       {
-        Serial.println(sum / oneup);     // sends the average over serial port.
+        Serial.println(sum / oneup);
       }
       else
       {
@@ -50,16 +50,16 @@ void loop()
         isdone = isdone + 1;
         Serial.println(isdone);
         delay(50);
-        Serial.println("done yet?");
+        Serial.println("Done yet?");
         if(isdone == 4)
         {
           delay(50);
-          Serial.println("washer done");  // send washer done over serial which the wifi board will interpret and send a message. 
-          washer = 0;                     // Reset the program to washer off. 
+          Serial.println("d");
+          washer = 0;
         }
       }  
       }
-    delay(100);  // check every 15 seconds
+    delay(15000);  // check every 15 seconds
     }
       
   delay(100);
